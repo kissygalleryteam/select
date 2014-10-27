ab567d91c05e9e9be3bf1692f602ee45967d7032
弘树 <tiehang.lth@alibaba-inc.com>
2014-10-27 16:47:55 +0800

    sync doc

---
+++ doc/build/mods-min.js
@@ -0,0 +1,2 @@
+/*!build time : 2014-08-11 3:48:31 PM*/
+KISSY.config("modules",{"kg/select/2.0.1/index":{requires:["node","menubutton","menu"]}});
---
+++ doc/build/mods.js
@@ -0,0 +1,3 @@
+KISSY.config('modules', {
+ 'kg/select/2.0.1/index': { requires: ['node', 'menubutton', 'menu']} 
+});
